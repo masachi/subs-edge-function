bba59e3188649922632eb590f8a4e761ce5b1105
masachi <zhangjb42@gmail.com>
2022-05-10 17:49:41 +0800

    edit api

--- pages/api/_middleware/index.js
+++ pages/api/_middleware/index.js
@@ -6,36 +6,32 @@ export async function middleware(req, ev) {
     const kancolleBaseUrl = "https://kancolle-subscription.herokuapp.com"
 
     const { pathname, search, hash } = req.nextUrl;
-  
+
     console.error("url", pathname, search, hash);
-  
+
     if (pathname.startsWith("/api")) {
-      return new Response(JSON.stringify({ pathname }), {
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-  
-    if (pathname.startsWith("/status")) {
-      const httpStatusCode = Number(pathname.split("/")[2]);
-  
-      return Number.isInteger(httpStatusCode)
-        ? fetch("https://http.cat/" + httpStatusCode)
-        : new Response("That's not a valid HTTP status code.");
-    }
-  
-    if (pathname.startsWith("/code")) {
-      const tenant = pathname.split("/")[2];
 
-      const code = pathname.split("/")[3];
+        if (pathname.startsWith("/api/status")) {
+            const httpStatusCode = Number(pathname.split("/")[2]);
 
-      if(tenant === 'kancolle') {
-        return fetch(`${kancolleBaseUrl}/${code}`);
-      }
+            return Number.isInteger(httpStatusCode)
+                ? fetch("https://http.cat/" + httpStatusCode)
+                : new Response("That's not a valid HTTP status code.");
+        }
 
-      if(tenant === 'newbarley') {
-        return fetch(`${nwebarleyBaseUrl}/${code}`);
-      }
+
+        if (pathname.startsWith("/api/kancolle/code")) {
+        
+            const code = pathname.split("/")[3];
+            return fetch(`${kancolleBaseUrl}/${code}`);
+        }
+
+        if (pathname.startsWith("/api/newbarley/code")) {
+
+            const code = pathname.split("/")[3];
+            return fetch(`${nwebarleyBaseUrl}/${code}`);
+        }
     }
-  
+
     return fetch("https://www.baidu.com/");
 }
